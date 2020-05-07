43977d575a3478945f73139e0a5d22055b4f578f
Qiaosen Huang <joesonw@gmail.com>
2020-05-07 15:02:13 +0800

    remove cluster name config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-CLUSTER_NAME=${CLUSTER_NAME:=cluster}
-
 if [[ "$HOSTNAME" =~ -0$ ]];
 then
   echo "Running in liftbridge-0 as a bootstrap seed..."
-  /liftbridge --config /etc/liftbridge.yaml --raft-bootstrap-seed --id="${CLUSTER_NAME}-$HOSTNAME"
+  /liftbridge --config /etc/liftbridge.yaml --raft-bootstrap-seed --id="$HOSTNAME"
 else
   echo "Running in $HOSTNAME..."
-  /liftbridge --config /etc/liftbridge.yaml --id="${CLUSTER_NAME}-$HOSTNAME"
+  /liftbridge --config /etc/liftbridge.yaml --id="$HOSTNAME"
 fi
